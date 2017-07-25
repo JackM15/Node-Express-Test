8fd5d63907bb8757704bef3bcbc849646b7bfe50
Jack <jackm550@googlemail.com>
2017-07-25 10:32:57 +0100

    Added Portfolio Page

--- server.js
+++ server.js
@@ -59,8 +59,14 @@ app.get("/about", (req, res) => {
 app.get("/bad", (req, res) => {
     res.send({
         errorMessage: "Error handling request."
-    })
+    });
 });
+
+app.get("/projects", (req, res) => {
+    res.render("projects.hbs", {
+        pageTitle: "Portfolio Page"
+    });
+})
 
 //start server (port number, console alert)
 app.listen(port, () => {
